f187270f007fb63fa6f109561a21a79cc6b7e94b
LuanaDfofa1021b <142016621+LuanaDfofa1021b@users.noreply.github.com>
2023-10-03 11:40:44 -0400

    bd

---
+++ lulu2029.js
@@ -0,0 +1,57 @@
+use("portifolio");
+db.createCollection("projetos");
+
+
+use("portifolio");
+db.projetos.insertMany([
+    {_id:1,nome:"Luana Delevatti",
+    descricao:"Olá, meu nome é Luana, tenho 17 anos e estou atualmente cursando o último ano do ensino médio no IFMS. Este portfólio será dedicado à apresentação da minha vida."
+},
+{_id:2,nome:"Minha Vida",
+descricao:" Gustavo Baraldi. Ele é a razão pela qual meu coração bate mais forte e minha vida está cheia de alegria. Gustavo transformou meus dias em uma jornada incrível de amor e felicidade. Estou profundamente grata por tê-lo na minha vida e ansiosa para criar muitas lembranças maravilhosas juntos no futuro."
+},
+{_id:3,nome:"Slinky",
+descricao:"Ele não é apenas um cachorro, ele é a razão do meu sorriso todos os dias. Com seus quatro anos de pura alegria e amor, Slinky ilumina cada canto da minha casa e enche meu coração de felicidade. Ele é um membro querido da nossa família, compartilhando conosco momentos de felicidades. "
+},
+{_id:4,nome:"Visita Técnica",
+descricao:"Esta foto incrível foi capturada na frente da usina em Foz do Iguaçu durante um projeto de dois dias em parceria com o IFMS . O projeto nos proporcionou a oportunidade única de explorar a imponente usina. Esses dois dias foram um conjunto de conhecimento. Esta foto agora se torna um lembrete dessa experiência inesquecível. "
+},
+{_id:5,nome:"Evento",
+descricao:"Esta foto foi tirada no casameneto da mãe da Vitória, foi um momento muito emocionante "
+},
+{_id:6,nome:"Melhor Amiga",
+descricao:"Essa pessoa da foto é a Vitoria, minha melhor amiga a quase 10 anos, nos conhecemos na escola no ensino fundamental e desde aquela epoca não nos largamos. "
+},
+{_id:7,nome:"Minha Prima",
+descricao:"Esta pessoa da foto é minha prima chamada Marcela, nos dias de hoje não somos muito proximas mas considero ela como uma irmã."
+},
+{_id:8,nome:"Karatê",
+descricao:"Esta foto foi tirada em 2016, um ano marcante em minha vida, quando eu estava fazendo karatê.Essa época foi verdadeiramente especial,que me ajudaram a crescer e evoluir como pessoa."
+},
+{_id:9,nome:"Academia",
+descricao:"Esta foto foi tirada na academia, no entanto, faz alguns meses que não tenho aparecido por lá. Estou sentindo falta desses dias de treinamento e espero em breve retomar minha rotina fitness."
+},
+{_id:10,nome:"Estágio",
+descricao:"Essa foto foi tirada no primeiro dia de estágio no centro de Convivência do Idoso Maria do Céu. "
+}
+
+])
+
+use("portifolio");
+db.createCollection("contatos");
+db.contatos.insertMany([
+    {nome:"Luana",email:"luana.delevatti@estudante.ifms.edu.br", mensagem:" que lindo, amei"
+},
+{nome:"Vitória",email:"vitória.santos9@estudante.ifms.edu.br", mensagem:" Uau, lindo"
+},
+{nome:"Felipe",email:"felipe.sarro@estudante.ifms.edu.br", mensagem:"ficou um pouco brega, mas está lindo"
+},
+{nome:"Matheus",email:"matheus.caetano@estudante.ifms.edu.br", mensagem:" Eita, que lindo",
+},
+
+
+
+])
+
+
+    
